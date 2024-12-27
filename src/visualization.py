55a143472d58ffe067751e8fe0afee8c684b1e43
mojtaba <mojtaba.mohamed187@gmail.com>
2024-12-27 18:40:48 +0200

    fix: Resolve visualization.py
    - Updated seaborn boxplot and violinplot to use orient instead of ert to avoid deprecation warnings.

--- src/visualization.py
+++ src/visualization.py
@@ -8,9 +8,9 @@ def plot_data(df, x_col, y_col, plot_type):
     plt.clf()  # Clear the previous plot to avoid overlap
 
     if plot_type == "Box Plot":
-        sns.boxplot(y=df[y_col], color="#d175ff")
+        sns.boxplot(y=df[y_col], color="#d175ff", orient="v")
     elif plot_type == "Violin Plot":
-        sns.violinplot(y=df[y_col], color="#d175ff")
+        sns.violinplot(y=df[y_col], color="#d175ff", orient="v")
     elif plot_type == "Bar":
         sns.countplot(y=y_col, data=df, color="#d175ff")
     elif plot_type == "Scatter Plot":
